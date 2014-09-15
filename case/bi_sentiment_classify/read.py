0961736f866fae2c126f6065136b6479446c6037
linhaobuaa <1257819385@qq.com>
2014-09-15 13:39:08 +0800

    little fix

--- case/bi_sentiment_classify/read.py
+++ case/bi_sentiment_classify/read.py
@@ -3,25 +3,12 @@
 import sys
 import json
 
-sys.path.append('./libsvm-3.17/python')
-from sta_ad import start
-
-f = open('items.jl')
+f = open('items_filter.jl')
 items_dict = {}
-items_mid_text = []
 for line in f:
     item = json.loads(line.strip())
     items_dict[item['_id']] = item
-    items_mid_text.append((item['_id'], item['text'].encode('utf-8')))
 f.close()
 
-print len(items_mid_text)
-mid_str_list = start(items_mid_text, '0915')
-print mid_str_list[:10]
-print len(mid_str_list)
-
-fw = open('items_filter.jl', 'wb')
-for mid in mid_str_list:
-    fw.write(json.dumps(items_dict[int(mid)]) + '\n')
-fw.close()
+print len(items_dict.keys())
 
