94589293e017f91684348e6e367e2cedfc1bc87e
linhaobuaa <1257819385@qq.com>
2014-09-19 04:19:58 +0800

    linhao little fix

--- case/identify/get_origin.py
+++ case/identify/get_origin.py
@@ -35,9 +35,12 @@ def acquire_user_by_id(uid):
 
 def get_origin_user(topic, end_date, windowsize):
     s = getXapianWeiboByTopic(topic)
+    query_dict = {
+        '$or': [{'message_type': 1}, {'message_type': 3}]
+    }
     weibos = []
-    get_results = s.iter_all_docs(fields=['_id', 'user', 'retweeted_uid', 'reposts_count', 'comments_count'])
-    for r in get_results:
+    count, get_results = s.search(query=query_dict, fields=['_id', 'user', 'retweeted_uid', 'reposts_count', 'comments_count'])
+    for r in get_results():
         w = dict()
         w['_id'] = r['_id']
         w['user'] = r['user']
@@ -51,11 +54,11 @@ def get_origin_user(topic, end_date, windowsize):
 
     sorted_weibos = sorted(weibos, key=lambda k: k[0], reverse=False)
     sorted_weibos.reverse()
-    #rank = 0
+
     origin_user_dict = {}
     origin_user_x = []
     user_list = []
-    for weibo in sorted_weibos:
+    for weibo in sorted_weibos[:50]:
         retweeted_uid = weibo[1]['retweeted_uid']
         count = s.search(query={'user': retweeted_uid}, count_only=True)
         if count:
